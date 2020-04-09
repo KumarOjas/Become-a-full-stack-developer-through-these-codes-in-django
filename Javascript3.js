3c5f53cda42041a74233a5c6fbac906b114b7fe7
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-10 00:30:23 +0530

    Create Javascript3.js

---
+++ Javascript3.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title> Html Document </title>
+<link href='css/style.css' rel= " stylesheet ">
+</head>
+<body>
+<h1 class='first-class'>The Heading</h1>
+<p class="basic-para">This is a paragraph</p>
+<p class="basic-para"   style="floor:right" > Another paragraph is here</p>
+<div style="clear:both;"></div>
+<p id="second-para">The second paragraph</p>
+<ul class="first-class">
+<li>One</li>
+<li>Two</li>
+<li>Three</li>
+</ul>
+<script>
+alert("This is an alert");
+alert("This second alert");
+</script>
+</body>
+</html>
+
+
