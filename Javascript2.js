e3c8c31857ad88b05238122b6a1ee2d30ccb333f
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-09 23:18:19 +0530

    Create Javascript2.js

---
+++ Javascript2.js
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Html Document</title>
+<link href="css/style.css" rel=" stylesheet ">
+</head>
+<body>
+<h1 class='first-class'> The heading </h1>
+<p class="basic-para">This is a paragraph </p>
+<p class="basic-para" style="float:right">Another paragraph is here </p>
+<div style="clear:both;"></div>
+<p id="second-para"> This second Paragrph </p>
+<ul class="first-class">
+<li> One </li>
+<li> Two </li>
+<li> Three </li>
+</ul>
+<script>
+alert("This is an alert");
+alert("The second  alert");
+</script>
+</body>
+</html>
