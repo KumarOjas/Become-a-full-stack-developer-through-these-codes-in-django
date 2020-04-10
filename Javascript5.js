28eb02d0e9c2659eb52843708b657f3469191be0
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-10 15:25:21 +0530

    Create Javascript5.js

---
+++ Javascript5.js
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title> Html Document</title>
+<link href=" css/style.css" rel="stylesheet">
+</head>
+<body>
+<h1 class="first-class">The heading </h1>
+<p class="basic-para">This is a paragraph </p>
+<p class="basic-para" style="float-right"> Another paragraph is here </p>
+<div style="clear:both;"></div>
+<p id="second para">The second paragraph</p>
+<ul class="first-class">
+<li>One </li>
+<li>Two </li>
+<li>Three </li>
+</ul>
+<script>
+int1=10;
+int2=20;
+Console.log(int1-int2);
+Console.log(int1*int2);
+Console.log(int1/int2);
+Console.log(int1 % int2);
+</script>
+</body>
+</html>
