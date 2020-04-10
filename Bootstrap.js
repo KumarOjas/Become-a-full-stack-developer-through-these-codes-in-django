d03fe59d1a2087c0bb9f5d3437d6389c19c237cb
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-10 20:03:27 +0530

    Create Bootstrap.js

---
+++ Bootstrap.js
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Learn bootstrap by Kumar Ojas<?title>
+<link rel="stylesheet"  href="css/bootstrap.min.css">
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col">
+<h1>Introduction</h1>
+<h1>class="display-1">Introduction </h1>
+<h2>class="display-2">Introduction </h2>
+<h3>class="display-3">Introduction </h3>
+<h4>class="display-4">Introduction </h4>
+</div>
+<div class="col">
+<h1>Heading<small>Summary</small></h1>
+</div>
+</div>
+</body>
+</html>
