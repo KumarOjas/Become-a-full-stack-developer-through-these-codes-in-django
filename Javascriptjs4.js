7444b5c94ce76748a2ed2985701f17d9cfdfa643
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-10 12:41:02 +0530

    Create Javascriptjs4.js

---
+++ Javascriptjs4.js
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title> Html Document </title>
+<link href="css/style.css"  rel="stylesheet" >
+</head>
+<body>
+<h1 class="first-class">The heading </h1>
+<p class ="basic-para">This is a paragraph</p>
+<p class ="basic-para" style="float:right"> Another paragraph is here</p>
+<div style="clear:both;"></div>
+<p id="second-para">The second Paragraph </p>
+<ul class="first-class">
+<li>One </li>
+<li>Two </li>
+<li>Three </li>
+</ul>
+<script>
+data="This is the variable data";
+int1="10";
+int2=20 ;
+txt1="10";
+txt2="20";
+str1= "There are ";
+str2="Some data in places";
+console.log("Logged data");
+console.log(4 + 5);
+console.log(data);
+console.log("int1 + int2");
+console.log("txt1 + txt2");
+console.log("int1 + 34");
+console.log("txt1 + 34");
+console.log( str1 + str2);
+console.log(str1 +"some bananas");
+console.log(int1 +"some data");
+</script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
