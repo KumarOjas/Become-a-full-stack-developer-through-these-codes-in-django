725fd16a2cec4d773b6d6e3af4eec1d79f442cf0
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-10 19:30:06 +0530

    Create Bootstrap1.js

---
+++ Bootstrap1.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Learn Bootstrap by Kumar Ojas </title>'
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"     integrity="sha384-Gn5384xqQrfrgerbgebegsrgberigeldsbfksbfwbfwrbgw "
+crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap4.0.0/js/bootstrap.min/js" integrity="bfsdvsbjsrgbsrgbrsigbrigeogheohthorjh"    crossorigin="anonymous "></script>
+<script src="css/bootstrap.min.js" integrity="sha384-32R65pejh4uufduqfdqfdwfwufewufg3u" crossorigin="anonymous"></script>
+</head>
+<body>
+<p>Hello World</p>
+<div class="jumbotron"></div>
+</body>
+</html>
