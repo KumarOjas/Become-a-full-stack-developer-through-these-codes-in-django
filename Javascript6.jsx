3d1be85c7d2fd1b8b307ba7ce77751988e5c43c8
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-10 15:37:20 +0530

    Create Javascript6.jsx

---
+++ Javascript6.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+class App extends React.Component {
+render () {
+var style ={
+  backgroundColor:"blue",
+  color:'white',
+  fontSize:'40px'
+  }
+  var var1=8;
+  return {
+  <div>
+  We are learning ReactJs
+  <div data-shattr="value">A nes Div </div>
+  <div style=[style]>Hello World </div>
+  <div>[4*4]</div>
+  <div>(var1  ==9 ?"True": "False" )</div>
+  {//Single line comment
+  }
+  {
+  /* Multi line Comment*/
+   }
+  </div>  
+
+};
+}
+}
+export default App;
