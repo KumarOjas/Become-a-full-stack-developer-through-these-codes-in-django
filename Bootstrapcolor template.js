0dbfde121bf0d8ef91dd8cb06c4d1a7cd1a1f33f
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-11 22:37:27 +0530

    Create Bootstrapcolor template.js

---
+++ Bootstrapcolor template.js
@@ -0,0 +1,56 @@
+<html>
+<head>
+<title>Learn bootstrap by Kumar Ojas</title>
+<link rel="stylesheet"  href="css/bootstrap.min.css" >
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col">
+<p class="text-primary">This is the paragraph </p>
+<p class="text-muted">This is the paragraph </p>
+<p class="text-warning">This is the paragraph </p>
+<p class="text-light">This is a paragraph </p>
+<p class="text-dark">This is a paragraph </p>
+<p class="text-danger">This is a paragraph </p>
+<p class="text-secondary">This is a paragraph </p>
+<p class="text-success">This is a paragraph </p>
+<p class="text-info">This is a paragraph </p>
+</div>
+<div class="col">
+<p class="text-light bg-primary">This is a paragraph</p>
+<p class="text-light bg-info">This is a paragraph</p>
+<p class="text-light bg-success">This is a paragraph</p>
+<p class="text-light bg-dark">This is a paragraph</p>
+<p class="text-light bg-light">This is a paragraph</p>
+<p class="text-light bg-warning">This is a paragraph</p>
+<p class="text-light bg-secondary">This is a paragraph</p>
+<p class="text-light bg-danger">This is a paragraph</p>
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
