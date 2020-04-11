9ad32a55cd86b9d73d8de44d6a4acdf6dc325f45
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-11 21:18:50 +0530

    Create Bootstrap containers.js

---
+++ Bootstrap containers.js
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Learn Bootstrap By Kumar Ojas </title>
+<link rel="stylesheet"  href="css/bootstrap.min.css" >
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="jumbotron">new Jumbotron</div>
+</div>
+<div class ="container fluid">
+<div class="jumbotron"> new Jumbotron</div>
+</div>
+<p> Hello World </p>
+<div class="jumbotron">Old Jumbotron </div>
+</body>
+</html>
+
