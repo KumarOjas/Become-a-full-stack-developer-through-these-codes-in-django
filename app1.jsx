0ff680f398de4aaa910984b4aa6f474067f0709e
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-11 14:21:29 +0530

    Create app1.jsx

---
+++ app1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+class App  extends React.Component {
+     render() {
+     return (
+     <div>
+     We're learning ReactJS
+     </div>
+     );
+     }
+     }
+     export default App;
