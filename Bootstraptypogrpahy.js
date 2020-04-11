4c0bb2d218e435ae1a6ca271371d0093be78de85
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 00:33:11 +0530

    Create Bootstraptypogrpahy.js

---
+++ Bootstraptypogrpahy.js
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Learn bootstrap by Kumar Ojas</title>
+<link rel="stylesheet" href=" css/bootstrap.min.css">
+<script src="js/jquery-min.js"></script>
+<script src="js/bootstrap-min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class="col">
+<p>THis is the <abbr ti tle="the main para">paragraph</abbr>.I would <mark>love </mark> to guide you what I can</p>
+<p>Hey all of you,there are some <code>codes </codes> !!</p>
+<p>By pressing  <kbd>Ctrl+ s </kbd>can save all your <span class="font-weight-light">Hard work</span> in <code> coding</code> </p>
+<p class="lead text-lift">A new paragraph is here </p>
+
+<p> Hey all of you, there are some <code> codes</code>!!</p>
+
+</div>
+</div>
+</body>
+</html>
