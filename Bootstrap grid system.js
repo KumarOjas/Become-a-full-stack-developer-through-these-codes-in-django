941d0c947b1058c9d99ee7d3416b050dbeee68b3
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-11 21:32:32 +0530

    Create Bootstrap grid system.js

---
+++ Bootstrap grid system.js
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>  Learn bootstrap by Kumar Ojas</title>
+<link rel="stylesheet"  href="css/bootstrap.min.css">
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="jumbotrun">new Jumbotrun</div>
+</div>
+<div class="container-fluid">
+<div class="jumbotrun">fluid Jumbotrun</div>
+</div>
+<p> Hello world </p>
+<div class="junction"> Old Jumbotron </div>
+</body>
+</html>
+
