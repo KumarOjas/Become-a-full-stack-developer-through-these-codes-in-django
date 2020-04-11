fd03ed51e7cb20fa95f3d0864086e5c9263f759b
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-11 22:24:55 +0530

    Create Bootstarap4Jumpbotron.js

---
+++ Bootstarap4Jumpbotron.js
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>Learn Bootstrap by Kumar Ojas </title>
+<link  rel="stylesheet"  href="css/bootstrap.min.css">
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<div class="row">
+</div>
+</div>
+<p> Hello World </p>
+<div class="jumbotron jumbotron-fluid">
+<div class="container">
+<h1>Heading </h1>
+</div>
+</div>
+< div class="jumbotron">
+< div class="container">
+<h1>Heading </h1>
+</div>
+</div>
+<h1>Heading </h1>
+</body>
+</html>
+
+
