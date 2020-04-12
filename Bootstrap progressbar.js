09cce0a9c10205bd74ab42c0d6b5268f0b156f5d
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 15:49:51 +0530

    Create Bootstrap progressbar.js

---
+++ Bootstrap progressbar.js
@@ -0,0 +1,76 @@
+<html>
+<head>
+<title>Learn bootstrap by Kumar Ojas </title>
+<link rel="stylesheet"  href="css/bootstrap.min.css">
+<script src="js/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<h1>Progress  bar </h1>
+<div class="progress"  style="height:100px";>
+<div class="progress-bar bg-warning" style="width:20%"height:100px;>20%</div>;      
+</div>
+
+<div class="progress"  style="height:100px"; >
+<div class="progress-bar bg-success" style="width:30%;height:100px;">30% </div>
+<div class="progress-bar bg-primary" style="width:30%;height:100px;">30% </div>
+<div class="progress-bar bg-success" style="width:30%;height:100px;">30% </div>
+</div>
+<div class="progress"  style="height:100px";>
+<div class="progress-bar" progress-bar-striped bg-info" style="width:30%;" height:100 px;>30%</div>
+</div>
+<div class="progress"  style="height:100px";>
+<div class="progress-bar" progress-bar-animated progress-bar-striped" style="width:30%;" height:100 px;>30%</div>
+</div>
+<div class="progress"  style="height:100px;" >
+
+<div class="progress-bar" progress-bar-animated progress-bar-striped" style="width:30%;" height:100 px;>30%</div>
+</div>
+<div class="progress"  style="height:100px;" >
+
+<div class="progress-bar" progress-bar-animated progress-bar-striped" style="width:30%;" height:100 px;>30%</div>
+</div>
+
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
