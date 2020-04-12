a59a0bd3022b0452f5d2b72b32c82eed7776a4df
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 13:00:47 +0530

    Create BootstrapBadges17.js

---
+++ BootstrapBadges17.js
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Learn Javascript by Kumar Ojas </title>
+<limk rel="stylesheet"   href=" css/bootstrap.min.css">
+<script src="js/jquery.js"></script>
+<script src="https:///cdjns.clourflame.com/ajax/libsgrsgrgrgrgggg.js"></script>
+<script src="js/bootstrap.min.js"> </script>
+</head>
+<body>
+<div class="container">
+<br>
+<span class='badge badge-secondary'>a badge</span>
+<span class='badge badge-secondary'>danger badge</span>
+<span class='badge badge-secondary'>primary badge</span>
+<span class='badge badge-secondary'> badge info </span>
+<span class='badge badge-secondary'> badge warning </span>
+<span class='badge badge-secondary'>Light badge</span>
+<span class='badge badge-secondary'>Dark badge</span>
+<h1>Heading 1<span class="badge badge-danger ">Heading budget</span></h1>
+<h2>Heading 1<span class="badge badge-danger ">Heading budget</span></h2>
+</div>
+</body>
+</html>
