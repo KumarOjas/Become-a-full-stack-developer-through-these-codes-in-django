b998e444c95462cf3b22816259b8cd658a04ee04
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 19:36:32 +0530

    Create BootstrapPaginations.js

---
+++ BootstrapPaginations.js
@@ -0,0 +1,56 @@
+<html>
+<head>
+<title>Learnbootstrap by kumar Ojas</title>
+<link rel="stylesheet" href="css/bootstrap.min.css" >
+<script src="js/jquery.js"></script>
+<script src="https://cdjns.cloudflare.com/ajax/libs/popper.js/12/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<ul class="pagination pagination-lg">
+<li class="page-item"><a href="#" class="page-link" >Previous</a> </li>
+<li class="page-item" active ><a href="#" class="page-link" ></a> </li>
+<li class="page-item"><a href="#" class="page-link" >3</a> </li>
+<li class="page-item"><a href="#" class="page-link" >4</a> </li>
+<li class="page-item  disabled "><a href="#" class="page-link" >Next</a> </li>
+</ul>
+<ul class="pagination ">
+<li class="page-item"><a href="#" class="page-link" >Previous</a> </li>
+<li class="page-item" active ><a href="#" class="page-link" >2</a> </li>
+<li class="page-item"><a href="#" class="page-link" ></a> </li>
+<li class="page-item"><a href="#" class="page-link" ></a> </li>
+<li class="page-item  disabled "><a href="#" class="page-link" >Next</a> </li>
+</ul>
+
+<ul class="pagination pagination-sm">
+<li class="page-item"><a href="#" class="page-link" >Previous</a> </li>
+<li class="page-item" active ><a href="#" class="page-link" >2</a> </li>
+<li class="page-item"><a href="#" class="page-link" >3</a> </li>
+<li class="page-item"><a href="#" class="page-link" >4</a> </li>
+<li class="page-item  disabled "><a href="#" class="page-link" >Next</a> </li>
+</ul>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
