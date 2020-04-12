e58e42459bdd44d42a181af16e3c8b2a47786004
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 13:53:15 +0530

    Create BootstrapBadges.js

---
+++ BootstrapBadges.js
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Learn bootstrap by Kumar Ojas</title>
+<link rel="stylesheet"   href="css/bootstrap.min.css ">
+<script src="js/jquery.js"></script>
+<script src="https://cdnjs.cloudfare.com/ajax/bgvyuguuu.js"></script>
+<script src="https://js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<spam class="badge badge-secondary">a badge </spam>
+<spam class="badge badge-danger">danger badge </spam>
+<spam class="badge badge-primary">primary badge </spam>
+<spam class="badge badge-danger"> badge info</spam>
+<spam class="badge badge-danger"> badge warning</spam>
+<spam class="badge badge-danger"> Light badge</spam>
+<spam class="badge badge-danger"> Dark badge </spam>
+
+<h1> Heading 1 <spam class="badge badge-danger">Heading badge</spam> </h1>
+<h2> Heading 2 <spam class="badge badge-danger">Heading badge</spam> </h2>
+</div>
+</body>
+</html>
