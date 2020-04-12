19da3022c40b91c8e9b8be8d183cf77c3e858f15
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 19:16:02 +0530

    Create Bootstraplistgroup.js

---
+++ Bootstraplistgroup.js
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Learn Javascript by Kumar Ojas</title>
+<link rel="stylesheet"  href="css/bootstrap.min.css">
+<script src="js/jquery.js"> </script>
+<script src="https://cdnjs.cloudfare.com/ajax/libs/popper.js/1.12/umd/propper.min.js"></script>
+<script src="js/bootstrap.min.js"> </script>
+</head>
+<body>
+<div class="container">
+<br>
+<div class="row">
+<div class="col-3">
+<ul class="list-group">
+   <li class="list-group-item">Item 1 </li>
+    <li class="list-group-item list-group-item-warning">Item 2 </li>
+    <li class="list-group-item list-group-item-">Item 3 </li>
+    <li class="list-group-item list-group-item-warning">Item 4 </li>
+    <li class="list-group-item list-group-item-warning">Item 5 </li>
+    
+ </ul>
+ </div>
+ <div class="col-3">
+ <div class="list-group ">
+ <a href="#" class="list-group-item-success  list-group-item-success">Item 1</a>
+  <a href="#" class="list-group-item-success  list-group-item-success">Item 2</a>
+   <a href="#" class="list-group-item-success  list-group-item-success">Item 3</a>
+   </div>
+   </div>
+   </div>
+   </body>
+   </html>
+   
+   
+   
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
