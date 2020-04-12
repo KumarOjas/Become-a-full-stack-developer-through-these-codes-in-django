f76f27a99ddf7aab5c9cdb0d9202a3d520615593
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 17:16:38 +0530

    Create Bootstraptables4.js

---
+++ Bootstraptables4.js
@@ -0,0 +1,87 @@
+<html>
+<head>
+<title> Learn bootstrap by Kumar Ojas </title>
+<link rel="stylesheet"  href="css/bootstrap.min.css">
+<script src="js/jquery.js"></script>
+<script src="https://sfrsfr.frsfrsfrg.com/ajax/libs/popper.js/1.12.9/umd/propper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<table class="table">
+<thread>
+<tr>
+<th>S.no</th>
+<th>Student name</th>
+<th>Student subject</th>
+<th>Student marks</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>1</td>
+<td>John</td>
+<td>Chemistry</td>
+<td>20</td>
+</tr>
+<tr>
+<td>2</td>
+<td>Hector</td>
+<td>Biology</td>
+<td>40</td>
+</tr>
+<tr>
+<td>3</td>
+<td>John</td>
+<td>Chemistry</td>
+<td>20</td>
+</tr>
+<tr>
+<td>1</td>
+<td>Eva</td>
+<td>Computer</td>
+<td>40</td>
+</tr>
+<tr>
+<td>3</td>
+<td>Eva</td>
+<td>Chemistry</td>
+<td>50</td>
+</tr>
+<tr>
+<td>4</td>
+<td>James</td>
+<td>Biology</td>
+<td>50</td>
+</tr>
+</tbody>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
