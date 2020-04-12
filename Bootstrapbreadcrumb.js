917818a787c7748423e6bcf8a66a014c26d48d08
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-12 20:21:33 +0530

    Create Bootstrapbreadcrumb.js

---
+++ Bootstrapbreadcrumb.js
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title> Learn Bootstrap by Kumar Ojas </title>
+<link rel="stylesheet"  href=""css/bootstrap.min.css">
+<script src="js/jquery.js"></script>
+<script src="https://cdjn.js.cloudflare.com/ajax/libs/popper.js/1.12.9/und/popper.min.js"></script>
+<script src=" js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<div class="card">
+<div class="card-body">The card body</div>
+</div>
+<br>
+<div class="card">
+<div class="card-header">The card header</div>
+<div class="card-body">The card body</div>
+<div class="card-footer">The card footer</div>
+</div>
+<br>
+<div class="card bg-danger">
+<div class="card-header">The header</div>
+<div class="card-body">The body</div>
+</div>
+<br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
