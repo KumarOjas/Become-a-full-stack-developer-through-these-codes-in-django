1f37d5720b5b58a771d1ecedc47f01d911a5a1c3
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-14 03:29:27 +0530

    Create Bootstrap4navbar.js

---
+++ Bootstrap4navbar.js
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Learn bootstrap by Kumar Ojas </title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script src="js/jquery.js"></script>
+<script src="https://cdjs.clouddflare.com/ajax/libs.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<ul class="nav nav-tabs">
+<li class="nav-item">
+<a href="#tab1" data-toggle="tab" class="nav-link active">Tab 1</a>
+</li>
+<li class="nav-item">
+<a href="#tab2"  class="nav-link" data-toggle="tab">Tab 2</a>
+</li>
+</ul>
+<div class="tab-content">
+<div class="tab-pane container active" id="tab-1" >
+This is the data of the tab 1
+</div>
+<div class="tab-pane container active" id="tab-2" >
+This is the data on the tab 2
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
