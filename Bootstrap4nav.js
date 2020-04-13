498d0cd4dacccd1b18e6ee4cbd5196e3dbbcd567
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-14 03:10:09 +0530

    Create Bootstrap4nav.js

---
+++ Bootstrap4nav.js
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title> Learn bootstrap by Kumar Ojas </title>
+<link  rel="stylesheet" href="css/bootstrap.min.css" >
+<script src="js/jquery.js"></script>
+<script src="https://cdjn.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<br>
+<ul class="nav">
+<li class="nav-item">
+<a href="#" class="new-link">Link 1</a>
+</li>
+<li class="nav-item">
+<a href="#" class="new-link">Link 2</a>
+</li>
+<li class="nav-item">
+<a href="#" class="new-link">Link 3</a>
+</li>
+</ul>
+<br>
+<ul class="nav flex-column">
+<li class="nav-item">
+<a href="#" class="nav-link">Link 1</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">Link 1</a>
+</li>
+<li class="nav-item">
+<a href="#" class="nav-link">Link 1</a>
+</li>
+</ul>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
