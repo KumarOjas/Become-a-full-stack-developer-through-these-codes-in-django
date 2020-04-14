682208bd8a9f355ebae24b60619ff15b2d1910a1
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-04-14 16:53:26 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+class App extends React.Component  {
+constructor(props) {
+super (props);
+{
+this.state={
+   header:"This is the header",
+   content:"  This is the content",
+   footer: "This  is the Footer"
+}
+}
+render() {
+return {
+<div>
+<div>
+<h1>this.state.header</h1>
+</div>
+<div>this.state.content</div>
+<div>this.state.footer</div>
+</div>
+);
+)
+)
+export deffault App;
